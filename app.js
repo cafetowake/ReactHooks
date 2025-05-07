23195f45db0f3ed14256e50f27f7f5a12378dd48
cafetowake <131809951+cafetowake@users.noreply.github.com>
2025-05-06 18:41:34 -0600

    empezar con el cronograma

---
+++ app.js
@@ -0,0 +1,10 @@
+function Stopwatch() {
+  return (
+    <div className="container">
+      <h1>Cronómetro</h1>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Stopwatch />);
