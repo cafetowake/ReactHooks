c29ebedcc7b7bd26a5e9b8259b9b075bd9e6464e
cafetowake <131809951+cafetowake@users.noreply.github.com>
2025-05-06 19:04:28 -0600

    ya funciona el contador

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 function Stopwatch() {
+    
+  const [time, setTime] = React.useState(0);
+  const [running, setRunning] = React.useState(false);
+  const intervalRef = React.useRef(null);
+
+  React.useEffect(
+    () => {
+    if (running) {
+      intervalRef.current = setInterval(() => {
+        setTime((seg) => seg + 1);
+      }, 1000);
+    } else if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    } 
+
+    return () => clearInterval(intervalRef.current);}, [running]
+  );
+
+  const toggleRunning = () => setRunning(!running);
+
   return (
     <div className="container">
       <h1>Cronómetro</h1>
+      <div className="time">{time}s</div>
+      <div className="buttons">
+        <button onClick={toggleRunning}>
+          {"Iniciar"}
+        </button>
+      </div>
     </div>
   );
 }
