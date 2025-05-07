cb01659dd9aab280633f78b0149e655d69b4e168
cafetowake <131809951+cafetowake@users.noreply.github.com>
2025-05-06 20:19:38 -0600

    se empieza la busqueda de personajes

---
+++ app.js
@@ -0,0 +1,10 @@
+const UserSearch = () => {
+    return (
+        <div className="container">
+            <h1>Búsqueda de Personajes de The Office</h1>            
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<UserSearch />);
