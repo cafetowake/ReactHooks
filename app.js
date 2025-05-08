6952deb3a2dd8b6c1cb041fe7bd7f027967069a5
cafetowake <131809951+cafetowake@users.noreply.github.com>
2025-05-08 02:06:24 -0600

    se empieza el Selector

---
+++ app.js
@@ -0,0 +1,11 @@
+function App() {
+
+    return (
+            <div className="app">
+                <h1>Selector claro o oscuro</h1>
+            </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
