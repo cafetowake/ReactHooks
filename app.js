ea7c1d5146d672b55287be8e8fbdd7f74579c34c
cafetowake <131809951+cafetowake@users.noreply.github.com>
2025-05-08 23:33:02 -0600

    se empieza el Renderizador

---
+++ app.js
@@ -0,0 +1,11 @@
+function CartElementos() {
+  return (
+    <div className="carta-elementos">
+      <h2>Carta de Elementos</h2>
+    </div>
+  );
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<CartElementos />);
