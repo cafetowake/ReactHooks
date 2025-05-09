4c062aefdfb784b18c215e69e4a8dcf6b008e5c9
cafetowake <131809951+cafetowake@users.noreply.github.com>
2025-05-08 23:46:33 -0600

    poner donde se pone el numero contador

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-function CartElementos() {
+function CartaElementos() {
+
+  const [counter] = React.useState(0);
+
   return (
     <div className="carta-elementos">
       <h2>Carta de Elementos</h2>
+      <p><strong>Contador:</strong> {counter}</p>
     </div>
   );
 }
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<CartElementos />);
+root.render(<CartaElementos />);
